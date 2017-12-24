b47e64af570272edd42a7ba46d2809fb49175ecf
Mohamad mehdi Kharatizadeh <m_kharatizadeh@yahoo.com>
2017-12-24 07:14:31 +0330

    added API key for heartbeat

--- cron/heartbeat.sh
+++ cron/heartbeat.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/bin/curl -k -X GET "https://api.rahansazeh.ucoder.ir/heartbeat/" -H "accept: */*"
+/bin/curl -k -X GET "https://api.rahansazeh.ucoder.ir/heartbeat/?api_key=5a3c28cca7f1a3000162fb27ce50570fb8a3449da15d06e9696efe61" -H "accept: */*"
 
